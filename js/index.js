8ba5adcf51423820821542091f6372ab18e8d95c
kim_young_geun <raintale92@naver.com>
2016-11-15 18:58:35 +0900

    두번째 커밋

---
+++ js/index.js
@@ -0,0 +1,18 @@
+/**
+ * Created by user on 2016-11-15.
+ */
+
+//자바스크립트 코드를 작성할 수 있음
+//alert("경고창")//자바 스크립트 코드
+
+var test = 100;
+var test2 = true;
+var test3 = "소리없는 아우성";
+var test4 = null;
+var test5;
+
+console.log(typeof test);
+console.log(typeof test2);
+console.log(typeof test3);
+console.log(typeof test4);
+console.log(typeof test5);
