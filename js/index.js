833ad44fa994283f23d45daa53751e2a8bf06d65
kim_young_geun <raintale92@naver.com>
2016-11-15 19:00:34 +0900

    second

--- js/index.js
+++ js/index.js
@@ -9,10 +9,9 @@ var test = 100;
 var test2 = true;
 var test3 = "소리없는 아우성";
 var test4 = null;
-var test5;
+
 
 console.log(typeof test);
 console.log(typeof test2);
 console.log(typeof test3);
 console.log(typeof test4);
-console.log(typeof test5);
