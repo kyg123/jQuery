d7adc537e02ac278e22f7c36819f9d0c28799711
kim_young_geun <raintale92@naver.com>
2016-11-16 20:37:49 +0900

    11/16

---
+++ js/jQuery03.js
@@ -0,0 +1,10 @@
+/**
+ * Created by user on 2016-11-16.
+ */
+
+//var test;
+
+function myFunc(){
+
+    alert($("option:selected").val());
+}
---
+++ js/jQuery04.js
@@ -0,0 +1,14 @@
+/**
+ * Created by user on 2016-11-16.
+ */
+
+//var test;
+
+function myFunc(){
+
+    //$("[type=checkbox]:checked").remove();
+    //$("[type=checkbox]:checked + span").remove();
+
+    $("[type=checkbox]:checked").parent().remove();
+}
+
